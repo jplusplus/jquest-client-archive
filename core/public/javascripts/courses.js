c4eaae0220d98ac50aa0f2d1dfe3f0df41b2ce2e
Pierre Romera <hello@pirhoo.com>
2012-12-04 21:09:37 +0100

    Restructured stylesheets

--- core/public/javascripts/courses.js
+++ core/public/javascripts/courses.js
@@ -4,9 +4,9 @@ new (function(window, undefined) {
 
 	that.initElements = function() {
 		return that.el = {
-			$chapters  			: $("#chapters"),
-      $myJourneyEmpty : $(".my-journey"),
-      $myJourney 			: $("#my-journey")
+			$myJourneyDynamic : $("#my-journey-dynamic"),
+      $myJourneyEmpty   : $(".my-journey"),
+      $myJourney 			  : $("#my-journey")
 		};		
 	};
 
@@ -23,9 +23,9 @@ new (function(window, undefined) {
 			
 			var $arrow = $(arrow)
 			// Find the current chapter
-			, $chapter = $arrow.parents(".chapter")
+			, $chapter = $arrow.parents(".chapter-card")
 			// Finds the parent
-			,  $parent = that.el.$chapters.find(".chapter[data-id=" + $arrow.data("parent") + "]")
+			,  $parent = that.el.$myJourney.find(".chapter-card[data-id=" + $arrow.data("parent") + "]")
 			// Finds the parent positions
 			, parentPosition = {
 				 	// Horizontal position
@@ -62,12 +62,33 @@ new (function(window, undefined) {
       // Load the journey with AJAX
       that.el.$myJourneyEmpty.loading().load("/courses/"+course+" #my-journey .span5", function() {   
 
-        var chaptersCount = that.el.$myJourneyEmpty.find(".span5").length + 1,
+        var chaptersCount = that.el.$myJourneyEmpty.find(".span5").length,
             chaptersWidth = that.el.$myJourneyEmpty.find(".span5").outerWidth();
 
-        that.el.$myJourneyEmpty.css("width", chaptersCount * chaptersWidth);
+        that.el.$myJourneyEmpty.css("width", (chaptersCount + 1) * chaptersWidth);
+
         that.initElements();  
-        //that.initArrows();
+
+        if( $.fn.iScroll ) {
+
+          // iScroll option
+          var iScrollOptions =  { 
+            hScrollbar: true, 
+            hScroll: true, 
+            snap: "li", 
+            momentum: false, 
+            scrollbarClass: 'iscrollbar', 
+            hideScrollbar:true,
+            fadeScrollbar:true
+          };
+
+          // Creates the iScroll instance
+          var $iscroll = that.el.$myJourneyDynamic.iScroll(iScrollOptions).css("overflow", "visible"),
+               $lastEl = that.el.$myJourneyDynamic.find("li:not(.disabled):last");       
+
+          $iscroll.data("iscroll").scrollToElement( $lastEl[0], 0 );
+        }
+
       });
 
     }
--- core/public/javascripts/global.js
+++ core/public/javascripts/global.js
@@ -9,12 +9,21 @@ new (function(window, undefined) {
     };
   };
 
-  that.adaptMainHeight = function() {
-    //that.el.$main.css("min-height", $(window).height() - that.el.$footer.outerHeight() - that.el.$main.css("padding-bottom")  );
-  };
-
   that.adaptMenuLayout = function() {    
     $(".navbar:first").toggleClass("at-top", ! $(window).scrollTop() );
+  };
+
+  /**
+   * Create an iscroll method to jquery if iscroll exists
+   * @return {Boolean} True if success
+   */
+  that.createJqueryIscroll = function() {    
+    if(typeof iScroll == "undefined") return false;
+    return !! ($.fn.iScroll = function(options) {
+      return $(this).each(function() { 
+        $(this).data("iscroll", new iScroll(this, options) ); 
+      });
+    });
   };
   
   $(that.init = function() {      
@@ -22,9 +31,10 @@ new (function(window, undefined) {
 
     that.adaptMenuLayout();
     $(window).on("scroll", that.adaptMenuLayout);
-
-    that.adaptMainHeight();
-    $(window).on("resize", that.adaptMainHeight);
+  
+    // Add iScroll as a jquery plugin
+    // If succed, create some iscroll instances
+    that.createJqueryIscroll();
 
   });
 
