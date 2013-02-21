28bbc49febe6e1dc8755fb565d6583a0f1e3da53
Pierre Romera <hello@pirhoo.com>
2013-02-21 14:38:59 +0100

    Set up a Tweet Manager for bot buster

--- custom/bot-buster/index.js
+++ custom/bot-buster/index.js
@@ -1,10 +1,9 @@
 // Dependencies
-var util      = require("util")
-, MissionQuiz = require("jquest-mission").MissionQuiz
-      , async = require("async")
-       , Twit = require("twit")
-          , _ = require("underscore")
-     , config = require("config");
+var       util = require("util")
+ , MissionQuiz = require("jquest-mission").MissionQuiz
+           , _ = require("underscore")
+      , config = require("config")
+, tweetManager = require("./tweetManager.js");
 
 // Tweets array
 var tweets = [];
@@ -18,9 +17,6 @@ module.exports = function(api, user, mission, callback) {
   self.addQuestion(getTweet);
   self.addQuestion(getTweet);
   self.addQuestion(getTweet);
-
-  var stream = twitterClient().stream('statuses/filter', { track: 'justin' })
-  stream.on("tweet", addTweetEvent)
 
   // Call the parent constructor
   module.exports.super_.call(self, api, user, mission, callback);
@@ -37,10 +33,10 @@ function getTweet(callback) {
   var waitForTweet = setInterval(function() {
 
     // Is there tweets ?
-    if(tweets.length > 0) clearInterval(waitForTweet);
+    if(tweetManager.count() > 0) clearInterval(waitForTweet);
     else return; // No tweet, continue to wait
 
-    var tweet = tweets[ _.random(0, tweets.length) ];    
+    var tweet = tweetManager.get( _.random(0, tweetManager.count()) );    
 
     callback(null, {
       label    : "Do you think this message was published by a human or by a robot?",
@@ -55,77 +51,9 @@ function getTweet(callback) {
 }
 
 /**
- * Event when the stream gets a tweet
- * @param {Object} tweet
- */
-function addTweetEvent(tweet) {
-  extendTweet(tweet, function(err, tweetExtended) {
-    if(! err) tweets.push(tweetExtended);
-  });
-}
-
-function extendTweet(tweet, callback) {
-
-  var tweetClone = _.clone(tweet);
-
-  // Twitter request option
-  var options = { 
-    id: tweet.id_str, 
-    align:"center", 
-    hide_thread: true,
-    omit_script: true,
-    hide_media: true
-  };
-
-  // Get the embed code first
-  twitterClient().get("statuses/oembed", options, function(err, oembed) {    
-    
-    // No embed code or error
-    if(err != null || !oembed) {
-      // Stop now
-      return callback({error: err || "No embed code."}, null);
-    }
-
-    tweetClone.oembed = oembed;
-    // Save the hashtags
-    tweetClone.hashtags = tweetClone.text.match(/#(\w+)/g);
-    // Removed every #
-    tweetClone.hashtags = _.map(tweetClone.hashtags, function(el) { return el.replace("#", "")});
-    // Save the mentions
-    tweetClone.mentions = tweetClone.text.match(/@(\w+)/g);
-    // Removed every @
-    tweetClone.mentions = _.map(tweetClone.mentions, function(el) { return el.replace("@", "")});
-
-    // extend the clone    
-    callback(null, tweetClone);
-  });
-
-}
-
-
-
-/**
- * Create a twitter client (instance of Twit) in this.twitterClient
- * @return {Twit} The twitterClient
- */
-function twitterClient() {
-
-  /**
-   * Twitter client
-   * @type {Twit}
-   */
-  return this.twitterClient || ( this.twitterClient = new Twit({
-      consumer_key        : config.oauth.consumers.twitter.consumerKey
-    , consumer_secret     : config.oauth.consumers.twitter.consumerSecret
-    , access_token        : config.oauth.consumers.twitter.accessToken
-    , access_token_secret : config.oauth.consumers.twitter.accessTokenSecret
-  }) );
-  
-}
-
-/**
  * Inheritance from "MissionQuizz"
  */
 util.inherits(module.exports, MissionQuiz);
+
 
 exports = module.exports;
