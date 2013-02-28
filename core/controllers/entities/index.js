a5a1fe3cf979d56ea47010b2a8ff7f900812e092
Pierre Romera <hello@pirhoo.com>
2013-02-28 20:16:36 +0100

    Added entity viewer

--- core/controllers/entities/index.js
+++ core/controllers/entities/index.js
@@ -17,4 +17,17 @@ module.exports = function(_app) {
 
   app = _app;
 
+  app.get("/:lang/e/:id", singleEntityPage);
+
 };
+
+
+var singleEntityPage = function(req, res) {
+
+    api.entity(req.params.id).get(function(err, entity) {
+        // No entity available
+        if(err) res.render("500", err);
+        else res.render("entities/entity", entity);
+    });
+
+}
